210f719fe9b9ba0212a455ebee6ffb0653f3b236
ccrisc <cesarina.criscione@gmail.com>
2024-08-05 10:26:15 +0200

    Fix

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -12,7 +12,7 @@ interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void;
 }
 
-interface TableData {
+type TableData = {
   price_abc: number;
   price_def: number;
   ratio: number;
@@ -20,7 +20,7 @@ interface TableData {
   upper_bound: number;
   lower_bound: number;
   trigger_alert: number;
-}
+};
 
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
@@ -65,9 +65,9 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      // Ensure generateRow returns data conforming to TableData type
-      const data = DataManipulator.generateRow(this.props.data);
-      this.table.update(data);
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),
+      ] as unknown as TableData[]);
     }
   }
 }
